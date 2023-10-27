01165fbf9dcaea92d5d20f2aff49ade2149fdf91
Jace Callihoo <jacecallihoo@gmail.com>
2023-10-26 21:46:43 -0600

    adding grid type

---
+++ life/grid.go
@@ -0,0 +1,15 @@
+package life
+
+type Grid[T any] struct {
+    data []T
+    rows int
+    cols int
+}
+
+func NewGrid[T any](rows, cols int) Grid {
+    var grid = Grid[T]{}
+    grid.data = make([]T, rows * cols)
+    grid.rows = rows
+    grid.cols = cols
+    return grid
+}
